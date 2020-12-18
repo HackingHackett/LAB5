2abd087543f5fc2224f54d36a2c0cd06dab305f0
hack <hack>
2020-12-18 01:50:20 -0800

    pseudo code

--- lab_5.py
+++ lab_5.py
@@ -1,16 +1,18 @@
-"""
-The prograsm is trying to translate a sentence captured as uder input.
-We first read in the text file textese.txt
-then from the text file, we create a list of strings from each lines in the text file.
-Then we create a dictinary the list.
-once the text file has been read into memory we close the file
-
-we then define a fucntion for trasnlating ehich envolves spliting the user input (sentence into an arroy of strings ("enjoy the excellent band tonight") ["enjoy, "the","excellent","band","tonight"]
-
-once we have the arry of string repsenting the user's input sentence we
-looop through each words, find the key matching the word and returns back the vaule tired to that word in our dictionary 
-After each word is trasnlated we then
-Print out the trasnlated sentance to the user.
-"""
-
 """ 
+main():
+ Set sentence = input()
+ set dictionary = create_dictionary()
+ traslate(sentence, dictionary)
+traslate(sentence, dictionary)
+ words = for each of the word in the sentence 
+ for each words, translate the word
+ print translated sentence to user 
+ create_dictionary():
+ read in textese.txt
+ create list = each line from file
+ close the file
+ create a dict off of the list
+ return the dict
+main()
+"""
+
